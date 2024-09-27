d89c7f17ce43382301e9e3015b23cd04e54d39a7
KatynOCtraining <t.theveupolaris@gmail.com>
2024-09-27 16:35:07 +0200

    loader

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ window.addEventListener("click", function(event) {
         modal.style.display = "none";
     }
 });
+
+
+// Attendre que la page soit complètement chargée
+window.onload = function() {
+    // Afficher le loader pendant 2.5 secondes
+    setTimeout(function() {
+      // Sélectionner l'élément du loader
+      const loaderWrapper = document.querySelector('.loader-wrapper');
+      
+      // Ajouter une classe pour déclencher une transition ou simplement le masquer
+      loaderWrapper.style.opacity = '0'; // Option pour une transition douce
+      
+      // Après 0.5s (le temps que l'opacité tombe à zéro), on cache totalement le loader
+      setTimeout(function() {
+        loaderWrapper.style.display = 'none';
+      }, 500); // Délai correspondant à la durée de la transition d'opacité
+    }, 2500); // Le loader reste visible pendant 2.5 secondes
+  };
+  
